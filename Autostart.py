297a3a22916b304e88ab07d4d85bd9f1fafaf3f3
TerraDev64 <TerraDev64@gmail.com>
2021-01-20 22:34:25 +0100

    removed testing comments

--- Autostart.py
+++ Autostart.py
@@ -12,6 +12,6 @@ print(userName)
 cPath = 'C:/Users/'
 path = cPath + userName
 if (((dt.datetime.today().weekday() + 1) < 5) and (cHour < 10 and cHour >6)):
-    #sp.Popen(path + '/AppData/Local/Microsoft/Teams/current/Teams.exe')
-    #sp.Popen('C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE')
+    sp.Popen(path + '/AppData/Local/Microsoft/Teams/current/Teams.exe')
+    sp.Popen('C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE')
     sp.Popen('C:/Program Files/Oracle/VirtualBox/VirtualBox.exe')
