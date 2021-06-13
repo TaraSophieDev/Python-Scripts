3a7b459062b889e04dcb1381efea4b4b3de46386
TerraDev64 <TerraDev64@gmail.com>
2021-06-13 10:10:21 +0200

    changed file name

--- pokemon_colors_to_csv.py
+++ pokemon_colors_to_csv.py
@@ -7,13 +7,22 @@ import colorgram as cg
 regular_folder = r"assets\pokemon_sprites\regular"
 shiny_folder = r"assets\pokemon_sprites\shiny"
 
+
+def write_csv(data):
+  with open('output\\regular_pokemon_colors.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+#header = ["pokemon_name", "r1", "g1", "b1", "r2", "g2", "b2", "r3", "g3", "b3", "r4", "g4", "b4", "r5", "g5", "b5" ,"r6", "g6", "b6"]
+
 #colors = cg.extract(r'assets\pokemon_sprites\regular\abra.png', 6)
-#print(colors)
+#print(colors)cle
 
 for r_sprites in os.listdir(regular_folder):
   path = r"assets\pokemon_sprites\regular"
   result = os.path.join(path, r_sprites)
   r_sprite_colors = cg.extract(result, 6)
+  print(r_sprite_colors)
+  write_csv(r_sprite_colors)
 
   for index in range(0, len(r_sprite_colors)):
     first_color = r_sprite_colors[index]
@@ -21,13 +30,10 @@ for r_sprites in os.listdir(regular_folder):
     r = rgb.r
     g = rgb.g
     b = rgb.b
-    print(r_sprites, "r",r, "g", g,"b", b)
-    print("index ",index + 1)
-  #print(r_sprites, r_sprite_colors)
-  #write_csv(r_sprites, r_sprite_colors)
+    #print(r_sprites,"index", index + 1, "r",r, "g", g,"b", b)
+    #write_csv(r_sprite_colors)
   #print(r_sprites, r_sprite_colors)
 
 
   #print("r" + "'" + regular_folder + r_sprites + "'")
-def write_csv(names ,colors):
-  index=["pokemon_name", "r1", "g1", "b1", "r2", "g2", "b2", "r3", "g3", "b3", "r4", "g4", "b4", "r5", "g5", "b5" ,"r6", "g6", "b6"]
+
